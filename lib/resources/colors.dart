ea21984bb41d05d95a5f23a0395fe6fe5699edfa
josemailson <jmailsons@gmail.com>
2023-08-22 09:14:52 -0300

    add: color pallete

---
+++ lib/resources/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const princitonOrange = Color(0xFFFE9000);
+  static const antiFlashWhite = Color(0xFFEEEDEC);
+  static const whiteSmoke = Color(0xFFF2F2F2);
+  static const platinum = Color(0xFFE3E3E3);
+  static const raisinBlack = Color(0xFF2B2325);
+}
